99c5f8ce06527f6f2000db004389920ceacf5718
Daac1115 <aaronaristega711@gmail.com>
2025-06-10 23:14:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('sobre').addEventListener('click', function() {
+    document.getElementById('sobre').style.display = 'none';
+    document.getElementById('regalo').classList.remove('hidden');
+});
